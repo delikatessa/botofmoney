03333be87f8db06c244bfb4ceeba9fbcb957278e
delikatessa <t.sandanova@gmail.com>
2017-06-25 15:04:11 +0200

    updated name

--- test/clojure_getting_started/web_test.clj
+++ test/clojure_getting_started/web_test.clj
@@ -1,6 +1,6 @@
-(ns clojure-getting-started.web-test
+(ns botofmoney.web-test
   (:require [clojure.test :refer :all]
-            [clojure-getting-started.web :refer :all]))
+            [botofmoney.web :refer :all]))
 
 (deftest first-test
   (is false "Tests should be written"))
